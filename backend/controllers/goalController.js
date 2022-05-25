101cc936883333d7affa8eda71591e7abe3ae63f
OzerOzturk <ozerozturk1636@gmail.com>
2022-05-26 00:21:33 +0300

    add goalController.

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,39 @@
+// @desc Get all goals
+// @route GET / api / goals
+// @access Private
+
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "Get goals" });
+};
+
+// @desc Set all goal
+// @route POST /api/goals
+// @access Private
+
+const setGoal = (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ message: "Set goal" });
+};
+
+// @desc Update all goal
+// @route PUT /api/goals/:id
+// @access Private
+
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+
+// @desc Delete all goal
+// @route DELETE /api/goals/:id
+// @access Private
+
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
