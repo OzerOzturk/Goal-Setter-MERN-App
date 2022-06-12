5cf6999545884ba86e8547094f06b3952897b7a8
OzerOzturk <ozerozturk1636@gmail.com>
2022-06-12 15:42:31 +0300

    add authenticate user.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -45,8 +45,6 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Invalid user data");
   }
-
-  res.json({ message: "Register User" });
 });
 
 // @desc Authenticate a user
@@ -54,6 +52,22 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access Public
 
 const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  //check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials, check your inputs");
+  }
+
   res.json({ message: "Login User" });
 });
 
