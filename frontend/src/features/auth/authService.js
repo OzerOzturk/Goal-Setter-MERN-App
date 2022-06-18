77a6e876f216e06231b9dacac0261dd55341f370
OzerOzturk <ozerozturk1636@gmail.com>
2022-06-18 13:58:05 +0300

    add logout,login functions.

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = "/api/users/";
 
-//Register use
+//Register user
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
 
@@ -13,8 +13,26 @@ const register = async (userData) => {
   return response.data;
 };
 
+//Login user
+const login = async (userData) => {
+  const response = await axios.post(API_URL + "login", userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+//logout user
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
 const authService = {
   register,
+  logout,
+  login,
 };
 
 export default authService;
