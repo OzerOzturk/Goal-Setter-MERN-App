aee2963cb82a17a7d1a15076452fb44842a4d327
OzerOzturk <ozerozturk1636@gmail.com>
2022-06-26 18:13:15 +0300

    add deleteGoal functionality.

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -28,9 +28,22 @@ const getGoals = async (token) => {
   return response.data;
 };
 
+//delete user goal
+const deleteGoal = async (goalId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await axios.get(API_URL + goalId, config);
+
+  return response.data;
+};
+
 const goalService = {
   createGoal,
   getGoals,
+  deleteGoal,
 };
 
 export default goalService;
